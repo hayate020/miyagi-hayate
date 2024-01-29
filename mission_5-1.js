52be125bc099880615c14d5a60c7b53b6c76adef
hayate020 <158068480+hayate020@users.noreply.github.com>
2024-01-30 01:23:03 +0900

    Add files via upload

---
+++ mission_5-1.js
@@ -0,0 +1,14 @@
+<script>
+var displayElement = document.getElementById('display');
+var currentExpression = '';
+    function appendnumber(number) {
+     currentExpression += number;
+     display.value = currentExpression;
+       
+    }
+    function appendOperator(operator) {
+     currentExpression += number;
+     display.value = currentExpression;
+       
+    }
+</script>
