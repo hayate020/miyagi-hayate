1cf4ef14955df67ff351b8cd06a00629a1785007
hayate020 <158068480+hayate020@users.noreply.github.com>
2024-01-30 01:30:33 +0900

    Update mission_5-1.js

--- mission_5-1.js
+++ mission_5-1.js
@@ -1,14 +1,1 @@
-<script>
-var displayElement = document.getElementById('display');
-var currentExpression = '';
-    function appendnumber(number) {
-     currentExpression += number;
-     display.value = currentExpression;
-       
-    }
-    function appendOperator(operator) {
-     currentExpression += number;
-     display.value = currentExpression;
-       
-    }
-</script>
+
